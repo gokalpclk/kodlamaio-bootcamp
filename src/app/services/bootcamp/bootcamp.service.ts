3ebec12ebcc4a2aea5f2966ff1b1fff9c87b7b7a
HabibeC <habibecelikten@gmail.com>
2022-11-30 17:11:58 +0300

    update bootcamp employee

--- src/app/services/bootcamp/bootcamp.service.ts
+++ src/app/services/bootcamp/bootcamp.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { IBootcampModel } from './../../models/bootcamp/BootcampModel';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BootcampService {
+  allBootcamps: IBootcampModel[] = [];
+  constructor(private httpClient: HttpClient) {}
 
-  constructor() { }
+  getAllBootcamps(){}
+  getBootcampById(){}
+  addBootcamp(){}
+  updateBootcamp(){}
+  deleteBootcamp(){}
 }
--- src/app/services/employee/employee.service.ts
+++ src/app/services/employee/employee.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { IEmployeeModel } from './../../models/employee/EmployeeModel';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EmployeeService {
-
-  constructor() { }
+  allEmployee: IEmployeeModel[] = [];
+  constructor(private httpClient: HttpClient) {}
+  getAllEmployees(){}
+  getEmployeeById(){}
+  addEmployee(){}
+  updateEmployee(){}
+  deleteEmployee(){}
 }
