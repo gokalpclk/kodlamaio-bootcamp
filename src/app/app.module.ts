27a35c8e6a4dad514cd4b2defdd5ab183b81a046
gokalpclk <06gokalp@gmail.com>
2022-11-30 12:19:54 +0300

    create project base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,14 @@ import { AppComponent } from './app.component';
 
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { EmployeeListComponent } from './components/employee/employee-list/employee-list.component';
+import { InstructorListComponent } from './components/instructor/instructor-list/instructor-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeListComponent,
+    InstructorListComponent
   ],
   imports: [
     BrowserModule,
