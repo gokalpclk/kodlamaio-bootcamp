bf0eb9ec72158b2ccbaed3aeebf5a280facc0038
Mustafa Peynirci <mustafapeynircigithub@gmail.com>
2022-12-01 16:14:25 +0300

    update applicant component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,5 @@
+import { ApplicantListComponent } from './components/applicant/applicant-list/applicant-list.component';
+import { ApplicantAddComponent } from './components/applicant/applicant-add/applicant-add.component';
 import { ApplicantUpdateComponent } from './components/applicant/applicant-update/applicant-update.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
@@ -5,7 +7,10 @@ import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: HomeComponent },
-  { path: 'update/:id', component: ApplicantUpdateComponent },
+  { path: 'applicant-list', component: ApplicantListComponent },
+  { path: 'applicant-add', component: ApplicantAddComponent },
+  { path: 'applicant-update/:id', component: ApplicantUpdateComponent },
+
 ];
 
 @NgModule({
