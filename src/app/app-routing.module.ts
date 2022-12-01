ab1389ac54e7f5ce9a580090d7e7241aab7c69ac
Mustafa Peynirci <mustafapeynircigithub@gmail.com>
2022-12-01 17:19:17 +0300

    update employeeComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,6 @@
+import { EmployeeUpdateComponent } from './components/employee/employee-update/employee-update.component';
+import { EmployeeAddComponent } from './components/employee/employee-add/employee-add.component';
+import { EmployeeListComponent } from './components/employee/employee-list/employee-list.component';
 import { ApplicantListComponent } from './components/applicant/applicant-list/applicant-list.component';
 import { ApplicantAddComponent } from './components/applicant/applicant-add/applicant-add.component';
 import { ApplicantUpdateComponent } from './components/applicant/applicant-update/applicant-update.component';
@@ -10,6 +13,13 @@ const routes: Routes = [
   { path: 'applicant-list', component: ApplicantListComponent },
   { path: 'applicant-add', component: ApplicantAddComponent },
   { path: 'applicant-update/:id', component: ApplicantUpdateComponent },
+  
+  { path: 'employee-list', component: EmployeeListComponent },
+  { path: 'employee-add', component: EmployeeAddComponent },
+  { path: 'employee-update/:id', component: EmployeeUpdateComponent },
+
+
+
 
 ];
 
--- src/app/components/employee/employee-list/employee-list.component.ts
+++ src/app/components/employee/employee-list/employee-list.component.ts
@@ -1,3 +1,5 @@
+import { EmployeeService } from './../../../services/employee/employee.service';
+import { IEmployeeAllModel } from './../../../models/employee/request/EmployeeAllModel';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeListComponent implements OnInit {
 
-  constructor() { }
+  allEmployeeList: IEmployeeAllModel[]=[]
+  constructor(private employeeService:EmployeeService) { }
 
   ngOnInit(): void {
+    // this.getAllEmployees();
   }
+
+  // getAllEmployees(){
+  //   this.employeeService.getAllEmployees().subscribe((data)=>{
+  //     this.allEmployeeList = data;
+  //   })
+  // }
 
 }
