43e73e6fced18733fdf04efd73b710f863dff1eb
gokalpclk <06gokalp@gmail.com>
2022-12-08 14:22:26 +0300

    bug fixed

--- src/app/layout/navi/navi.component.ts
+++ src/app/layout/navi/navi.component.ts
@@ -5,34 +5,20 @@ import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 // import { Logout } from 'src/app/store/actions/user-actions';
 
-
 @Component({
   selector: 'app-navi',
   templateUrl: './navi.component.html',
   styleUrls: ['./navi.component.css'],
 })
 export class NaviComponent implements OnInit {
-  constructor(private router: Router, 
-    private store: Store<any>
-    ) {}
+  constructor(private router: Router, private store: Store<any>) {}
 
-    user:IUser;
-  ngOnInit(): void {
-    this.getUser()
-  }
+  ngOnInit(): void {}
   logOut() {
     // this.store.dispatch(new Logout())
     // redux
     localStorage.clear();
     this.store.dispatch(new Logout());
     this.router.navigate(['']);
-    this.getUser();
-  }
-  getUser() {
-    this.store.select('userReducer').subscribe((state) => {
-      this.user = state;
-      console.log(this.user);
-      
-    });
   }
 }
