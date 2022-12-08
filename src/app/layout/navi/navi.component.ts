ad426e4ddd55c281db5c5c62799f9978ab9a1735
HabibeC <habibecelikten@gmail.com>
2022-12-08 16:40:52 +0300

    update bootcamp

--- src/app/layout/navi/navi.component.ts
+++ src/app/layout/navi/navi.component.ts
@@ -5,21 +5,34 @@ import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 // import { Logout } from 'src/app/store/actions/user-actions';
 
+
 @Component({
   selector: 'app-navi',
   templateUrl: './navi.component.html',
   styleUrls: ['./navi.component.css'],
 })
 export class NaviComponent implements OnInit {
-  constructor(private router: Router, private store: Store<any>) {}
+  constructor(private router: Router, 
+    private store: Store<any>
+    ) {}
 
-  ngOnInit(): void {}
+    user:IUser;
+  ngOnInit(): void {
+    this.getUser()
+  }
   logOut() {
     // this.store.dispatch(new Logout())
     // redux
     localStorage.clear();
     this.store.dispatch(new Logout());
     this.router.navigate(['']);
+    this.getUser();
   }
-
+  getUser() {
+    this.store.select('userReducer').subscribe((state) => {
+      this.user = state;
+      console.log(this.user);
+      
+    });
+  }
 }
